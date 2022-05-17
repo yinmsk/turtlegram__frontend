6a99ec527ce83d2e35040514919c2a59dd6f7a89
yinmsk <galaxy220111@gmail.com>
2022-05-17 11:50:34 +0900

    host주소 5000으로 변경

--- api.js
+++ api.js
@@ -1,15 +1,20 @@
 // 비동기 함수라는걸 알려주기 위해 async를 사용
 async function handleSignin() {
+    console.log('here')
 
     const signupData = {
+        // html의 floatingInput와 floatingPassword를 가져와 준다
         email: document.getElementById("floatingInput").value,
         password: document.getElementById("floatingPassword").value
     }
+    console.log(signupData)
+
 
 
     // await를 사용하므로 여기부터 비동기라는걸 알려준다
-    const response = await fetch('http:127.0.0.1:5500/signup', {
+    const response = await fetch('http://127.0.0.1:5000/signup', {
         method: 'POST',
+        // 위의 const signupData를 가져온다
         body: JSON.stringify(signupData)
     }
     )
