e38419c77f7648012d4ac544963a374ff33a671d
yinmsk <galaxy220111@gmail.com>
2022-05-18 21:10:36 +0900

    로그인 페이지에서 토큰 받아오기 완료 js에서 https 주소 디버깅함

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ const frontend_base_url = "http://127.0.0.1:5501"
 
 // 비동기 함수라는걸 알려주기 위해 async를 사용
 async function handleSignin() {
+
     const signupData = {
         // html의 floatingInput와 floatingPassword를 가져와 준다
         email: document.getElementById("floatingInput").value,
@@ -18,7 +19,7 @@ async function handleSignin() {
     }
     )
     console.log(response)
-
+    // _json???????????????????????
     response_json = await response.json()
     console.log(response_json)
 
@@ -34,6 +35,7 @@ async function handleSignin() {
 
 async function handle_login() {
     console.log("handle_login")
+
     const loginData = {
         email: document.getElementById("floatingInput").value,
         password: document.getElementById("floatingPassword").value
@@ -41,12 +43,14 @@ async function handle_login() {
 
 
     // await를 사용하므로 여기부터 비동기라는걸 알려준다/////////////5000번 주소 찾을 수 없다고 나오는것 같다
-    const response = await fetch(`${backend_base_url}/signup`, {
+    const response = await fetch(`${backend_base_url}/login`, {
         method: 'POST',
         // 위의 const signupData를 가져온다
         body: JSON.stringify(loginData)
     }
     )
+
+
     console.log(response)
 
     response_json = await response.json()
@@ -55,18 +59,12 @@ async function handle_login() {
     // ??????
     localStorage.setItem("token", response_json.token)
 
-
-    if (response.status == 200) {
-        window.location.response("http://127.0.0.1:5501/login.html");
-    }
-    else {
-        alert(response.status)
-    }
 }
 
 
 
 async function getName() {
+
     const response = await fetch(`${backend_base_url}/getuserinfo`, {
         headers: {
             'Authorization': localStorage.getItem("token")
@@ -74,6 +72,7 @@ async function getName() {
     }
     )
     response_json = await response.json()
+    console.log(response_json)
 
     // $(#username)과 비슷하다 document는 html이라는 뜻이다
     const username = document.getElementById("username")
