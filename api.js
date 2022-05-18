259ba4d046d116660f7af68d512b86c448ed5437
yinmsk <galaxy220111@gmail.com>
2022-05-18 16:43:19 +0900

    로그인 페이지 js 추가

--- api.js
+++ api.js
@@ -1,15 +1,11 @@
 // 비동기 함수라는걸 알려주기 위해 async를 사용
 async function handleSignin() {
-    console.log('here')
-
     const signupData = {
         // html의 floatingInput와 floatingPassword를 가져와 준다
         email: document.getElementById("floatingInput").value,
         password: document.getElementById("floatingPassword").value
     }
     console.log(signupData)
-
-
 
     // await를 사용하므로 여기부터 비동기라는걸 알려준다
     const response = await fetch('http://127.0.0.1:5000/signup', {
@@ -19,6 +15,25 @@ async function handleSignin() {
     }
     )
 
-
     console.log(response)
+}
+
+
+
+async function handle_login() {
+    console.log("handle_login")
+    //     const signupData = {
+    //         email: document.getElementById("floatingInput").value,
+    //         password: document.getElementById("floatingPassword").value
+    //     }
+    //     console.log(signupData)
+
+    //     const response = await fetch('http://127.0.0.1:5000/login', {
+    //         method: 'POST',
+    //         body: JSON.stringify(signupData)
+    //     }
+    //     )
+
+
+    //     console.log(response)
 }
