4fa663ed0c70cef200f7047962641ab1d1d23160
yinmsk <galaxy220111@gmail.com>
2022-05-20 10:06:19 +0900

    CLN

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ async function loadArticles() {
     console.log("here")
     articles = await getArticles()
     console.log(articles)
-    // const article_list = document.getElementById("articles")
+    const article_list = document.getElementById("articles")
 
-    // articles.forEach(article => {
-    //     console.log(article)
-    //     const newArticle = document.createElement("li");
-    //     newArticle.setAttribute("id", article._id)
-    //     newArticle.innerText = article.title
-    //     article_list.appendChild(newArticle)
+    articles.forEach(article => {
+        console.log(article)
+        const newArticle = document.createElement("li");
+        newArticle.setAttribute("id", article._id)
+        newArticle.innerText = article.title
+        article_list.appendChild(newArticle)
 
-    // });
+    });
 
 
 
